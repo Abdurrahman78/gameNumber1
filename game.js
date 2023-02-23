6e84f95f1a08ed3f3823c8cbe00cd5438b697ae1
Abdurrahman Elkhadrawy <abdurrahmanelkhadrawy@abdurrahmansmbp.mynetworksettings.com>
2023-02-22 20:55:04 -0500

    Finished logic implementaion Maybe Add styles later

--- game.js
+++ game.js
@@ -9,11 +9,21 @@ let scissorsButton=document.querySelector('.scissors')
 
 let displayMessage=document.querySelector('.display')
 
+let scorePlayer=document.querySelector('.PlayerScore')
+
+let scoreComputer=document.querySelector('.ComputerScore')
+
+let results=document.querySelector('.results')
+
+let playAgainButton=document.querySelector('.playagain')
+
+playAgainButton.classList.add('none');
 
 //will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 let computerScore = 0;
 let playerScore = 0;
 let draw = 0;
+
 
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
@@ -31,35 +41,71 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
     displayMessage.innerHTML='Player has won! Rock beats scissors'
+    scorePlayer.innerHTML=`${playerScore}`
+
   }
   else if (computerSelection === "rock" && playerSelection === "scissors") {
     computerScore++;
     displayMessage.innerHTML='Computer has won! Rock beats scissors'
+    scoreComputer.innerHTML=`${computerScore}`
+
   }
   else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
     displayMessage.innerHTML='Computer has won! Paper beats Rock'
+    scoreComputer.innerHTML=`${computerScore}`
+
   }
   else if (computerSelection === "rock" && playerSelection === "paper") {
     playerScore++;
     displayMessage.innerHTML='Player has won! Paper beats Rock'
+    scorePlayer.innerHTML=`${playerScore}`
+
   }
   else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
     displayMessage.innerHTML='Player has won! Scissors beats Paper'
+    scorePlayer.innerHTML=`${playerScore}`
+
   }
   else if (computerSelection === "scissors" && playerSelection === "paper") {
     computerScore++;
     displayMessage.innerHTML='Computer has won! Scissors beats Paper'
+    scoreComputer.innerHTML=`${computerScore}`
+
   }
   else {
     draw++;
     displayMessage.innerHTML=`Both players chose ${playerSelection}. It was a Draw!`
   }
 
+checkWin(playerScore,computerScore);
 }
 
+function checkWin(player,computer){
+
+  if(player===5){
+    results.innerHTML="Player has Won the game!"
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+    playAgainButton.classList.remove('none');
+  }
+  else if(computer===5){
+    results.innerHTML="Computer has Won the game!"
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+    playAgainButton.classList.remove('none');
+  }
+  
+  }
+
+function playAgain(){
+  location.reload();
+}
 
 rockButton.addEventListener('click',()=>playRound("rock",getComputerChoice()))
 paperButton.addEventListener('click',()=>playRound("paper",getComputerChoice()))
 scissorsButton.addEventListener('click',()=>playRound("scissors",getComputerChoice()))
+playAgainButton.addEventListener('click',()=>playAgain())
